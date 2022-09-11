90705505db9c51c3bf1bca8f2c3cce19d65252d6
JSmith <jaysmit158@gmail.com>
2022-09-10 22:12:53 -0400

    Use client only

--- components/EmailModal.vue
+++ components/EmailModal.vue
@@ -7,40 +7,42 @@
 		<div class="formWrapper absolute">
 			<button class="float-right font-bold green text-2xl" @click="close"> Close &times; </button>
 			<div class="clear-both"></div>
-			<form 
-				id="emailForm" 
-				name="Contact Me - FFE Portfolio" 
-				method="POST" 
-				data-netlify="true"
-				data-netlify-recaptcha="true"
-				data-netlify-honeypot="bot-field"
-				class="bg-white p-4 rounded"
-			>
-				<div class="mb-2">
-					<label for="eName" class="w-full">Your Name:</label>
-					<input id="eName" class="w-full border border-teal-800" name="name" type="text" placeholder="Your Name..." v-model="nameVal" />
-				</div>
-				<div class="mb-2">
-					<label for="eAddress" class="w-full">Email:</label>
-					<input id="eAddress" name="email" class="w-full border border-teal-800" type="email" placeholder="Your Email..." v-model="emailVal" />
-				</div>
-				<div>
-					<label for="message" class="w-full">Your Message:</label>
-					<textarea 
-						id="message" 
-						name="message" 
-						rows="4" 
-						class="w-full border border-teal-800"
-						placeholder="Your Message..." 
-						v-model="messageVal"
-					></textarea>
-				</div>
-				<div class="hidden">
-					<input name="bot-field" />
-				</div>
-				<div data-netlify-recaptcha="true" class="recap-block m-2"></div>
-				<button type="submit" class="bg-teal-800 mt-4 text-white w-full p-1">Submit</button>
-			</form> 
+			<clent-only>
+				<form 
+					id="emailForm" 
+					name="Contact Me - FFE Portfolio" 
+					method="POST" 
+					data-netlify="true"
+					data-netlify-recaptcha="true"
+					data-netlify-honeypot="bot-field"
+					class="bg-white p-4 rounded"
+				>
+					<div class="mb-2">
+						<label for="eName" class="w-full">Your Name:</label>
+						<input id="eName" class="w-full border border-teal-800" name="name" type="text" placeholder="Your Name..." v-model="nameVal" />
+					</div>
+					<div class="mb-2">
+						<label for="eAddress" class="w-full">Email:</label>
+						<input id="eAddress" name="email" class="w-full border border-teal-800" type="email" placeholder="Your Email..." v-model="emailVal" />
+					</div>
+					<div>
+						<label for="message" class="w-full">Your Message:</label>
+						<textarea 
+							id="message" 
+							name="message" 
+							rows="4" 
+							class="w-full border border-teal-800"
+							placeholder="Your Message..." 
+							v-model="messageVal"
+						></textarea>
+					</div>
+					<div class="hidden">
+						<input name="bot-field" />
+					</div>
+					<div data-netlify-recaptcha="true" class="recap-block m-2"></div>
+					<button type="submit" class="bg-teal-800 mt-4 text-white w-full p-1">Submit</button>
+				</form> 
+			</client-only>
 		</div>
 	</div>
 </template>
